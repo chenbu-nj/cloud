dce0c63eded876056c83169392a9f0e2bd454bd8
xueshize <xueshize_nj@163.com>
2021-04-20 17:20:02 +0800

    springcloud伪集群配置提交

--- clienta/src/main/java/com/example/clienta/controller/RedirectController.java
+++ clienta/src/main/java/com/example/clienta/controller/RedirectController.java
@@ -2,22 +2,31 @@ package com.example.clienta.controller;
 
 import com.example.clienta.service.GetTransIdService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+@RefreshScope
 @RestController
 public class RedirectController {
 
     @Autowired
     private GetTransIdService getTransIdService;
 
+    @Value("${server.port}")
+    String port;
+
+    @Value("${test.flag}")
+    String flag;
+
     @GetMapping(value = "straightGetTransId")
     @ResponseBody
     public String straightGetTransId(){
-        return "服务A接口：" + UUID.randomUUID().toString();
+        return "服务A:" + port + "接口：" + UUID.randomUUID().toString() + ",标识：" + flag;
     }
 
     @GetMapping(value = "redirectGetTransId")
